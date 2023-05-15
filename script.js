465e2db681e2b2a767389654739c2f8e5282abec
rafa.gutierrez19 <rafaelgutierrezmorales.17@gmail.com>
2023-05-15 17:08:03 -0600

    tercer cambio

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-document.querySelector("#check").addEventListener("click",function(){
-	let bulb = document.querySelector("#light");
-	if(this.checked)
-		bulb.classList.add("on");
-	else
-		bulb.classList.remove("on");
-});
+let menu = document.jquerySelector('#menu-bars');
+let navbar = document.jquerySelector('.navbar');
+
+menu.onclick = () =>{
+	menu.classList.toogle('fa-times');
+	menu.classList.toogle('active');
+}
+window.onscroll = () =>{
+	menu.classList.remove('fa-times');
+	menu.classList.remove('active');
+}
+document.jquerySelector('#search-icon').onclick = () =>{
+	document.jquerySelector('#search-icon').classList.onclick = ()=>{
+
+	}
+}
+
+
