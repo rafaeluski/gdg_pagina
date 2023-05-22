5bf10b82ced685e488f9b1ac6a25c7bb63e0e124
rafa.gutierrez19 <rafaelgutierrezmorales.17@gmail.com>
2023-05-22 17:00:57 -0600

    avance

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
 let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
+
 menu.onclick = () => {
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-    
+    menu.classList.toogle('fa-times');
+    navbar.classList.toogle('active');
 }
-window.onscroll=()=>{
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-}
-var swiper = new swiper('.home-slider',{
-    spaceBetween:30,
+var swiper = new swiper (".home-slider",{
+    spaceBetween: 30,
     centeredSlides: true,
     autoplay:{
-        delay:7500,
+        delay: 500,
         disableOnInteraction: false,
-    },
-    pagination:{
-        el:".swiper-pagination",
-        clickable: true,
-    },
-   loop:true,
-   
-});
+},
+pagination:{
+el: ".swiper-pagination",
+clickable: true,
+},
+loop:true,
+}); 
+$('ul.tabs li a:first').addClass('active'); 
+	$('.secciones article').hide();
+	$('.secciones article:first').show();
+
+	$('ul.tabs li a').click(function(){
+		$('ul.tabs li a').removeClass('active');
+		$(this).addClass('active');
+		$('.secciones article').hide();
+
+		var activeTab = $(this).attr('href');
+		$(activeTab).show();
+		return false;
+	});
+
+document.querySelector("#check").addEventListener("click",function(){
+        let bulb = document.querySelector("#light");
+        if(this.checked)
+            bulb.classList.add("on");
+        else
+            bulb.classList.remove("on");
+    });
