3a7e69acbb03998a08413ac4ad628bcea0d41abc
rafa.gutierrez19 <rafaelgutierrezmorales.17@gmail.com>
2023-05-24 18:00:27 -0600

    bueno

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var swiper = new swiper (".home-slider",{
     spaceBetween: 30,
     centeredSlides: true,
     autoplay:{
-        delay: 500,
+        delay: 7500,
         disableOnInteraction: false,
 },
 pagination:{
@@ -18,24 +18,8 @@ clickable: true,
 },
 loop:true,
 }); 
-$('ul.tabs li a:first').addClass('active'); 
-	$('.secciones article').hide();
-	$('.secciones article:first').show();
+$(document).ready(function(){
+	$('ul.tabs li a:first').addClass('active');
+});
 
-	$('ul.tabs li a').click(function(){
-		$('ul.tabs li a').removeClass('active');
-		$(this).addClass('active');
-		$('.secciones article').hide();
 
-		var activeTab = $(this).attr('href');
-		$(activeTab).show();
-		return false;
-	});
-
-document.querySelector("#check").addEventListener("click",function(){
-        let bulb = document.querySelector("#light");
-        if(this.checked)
-            bulb.classList.add("on");
-        else
-            bulb.classList.remove("on");
-    });
